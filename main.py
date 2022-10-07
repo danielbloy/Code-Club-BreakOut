e92a5e6abf1c0fb2922b7f9b187d3aef4d39e8b1
Daniel Bloy <daniel_bloy@hotmail.com>
2022-10-07 17:04:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+live = 3
+root = tk.Tk()
+frame = tk.Frame(root)
+canvas = tk.Canvas(frame, width=600,height=400,bg='#aaaaff')
+frame.pack()
+canvas.pack()
+root.title('Hello, Pong!')
+root.mainloop()
+
